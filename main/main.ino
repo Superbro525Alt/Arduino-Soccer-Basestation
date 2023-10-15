d8e66d63299a9baec9f92fc78e1d356fefe9e955
= <=>
2023-10-15 19:57:38 +0800

    Added radio recieve code

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,32 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const byte address[5][6] = {"00001", "00002", "00003", "00004", "00005"};
+
+int amt_transmitters = 5;
+RF24 radios[5] = {};
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  for (int i = 0; i < amt_transmitters; i++) {
+    RF24 radio(7, 8);
+    radios[i] = radio;
+    radios[i].begin();
+
+    radio.openReadingPipe(0, address[i]);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.startListening();
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-
-  
+  for (int i = 0; i < amt_transmitters; i++) {
+    if (radios[i].available()) {
+      char text[32] = "";
+      radios[i].read(&text, sizeof(text));
+      Serial.println(text);
+    }
+  }
 }
